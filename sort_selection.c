f25e0f774d785a283435fb9eac20858de8a67b42
duaa234 <107420796+duaa234@users.noreply.github.com>
2023-10-04 16:37:34 +0530

    Create sort_selection.c

---
+++ sort_selection.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,min,temp;
+    int a[6]={9,3,5,6,1,4};
+    for(i=1;i<6;i++)
+    {
+        min = i;
+        for(j=0;j<6;j++)
+        {
+            if(a[j] > a[min])
+            {
+                min = j;
+            }
+        }
+        temp = a[min];
+        a[min]=a[i];
+        a[i]= temp;
+    }
+    for(i=0;i<6;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+}
